64ec5899397e934eff6e60278007c858ca7679fe
nonznonz <nonznonz25@gmail.com>
2024-04-28 01:15:19 +0200

    additional weather information added

--- styles/index.js
+++ styles/index.js
@@ -1,13 +1,67 @@
+function refreshWeather(response){
+
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature =  response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time*1000);
+
+
+
+    cityElement.innerHTML = response.data.city;
+
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%;`
+
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+    temperatureElement.innerHTML = Math.round(temperature)
+    
+}
+
+
+function formatDate(date){
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let days = ["Sun","Mon", "Tues", "Wed", "Thurs","Fri","Sat"];
+    
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
+function searchCity(city){
+    let apiKey = "cbf1d504ac4o03023039f403acda1bat";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+   axios.get(apiUrl).then(refreshWeather);
+}
+
+
 
 function handleSearch(event){
 
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Paris");
